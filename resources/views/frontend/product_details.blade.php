84bfe651891c05aedf0cfe90b3181ebce31cf8fd
bnmanish <bnmanish006@gmail.com>
2025-11-29 10:55:09 +0530

    changes

--- resources/views/frontend/product_details.blade.php
+++ resources/views/frontend/product_details.blade.php
@@ -83,7 +83,7 @@
 
 					<div class="row">
 
-						<div class="col-md-7 col-lg-9 col-xl-9">
+						<div class="col-md-12">
 							<!-- Doctor Widget -->
 							<div class="card">
 								<div class="card-body product-description">
@@ -234,89 +234,6 @@
 							</div>
 							<!-- /Doctor Details Tab -->
 
-						</div>
-
-						<div class="col-md-5 col-lg-3 col-xl-3 theiaStickySidebar">
-							
-							<!-- Right Details -->
-							<div class="card search-filter">
-								<div class="card-body">
-									<div class="clini-infos mt-0">
-										<h2>$19 <b class="text-lg strike">$45</b>  <span class="text-lg text-success"><b>10% off</b></span></h2>
-									</div>
-									<span class="badge badge-primary">In stock</span>
-									<div class="custom-increment pt-4">
-	                                    <div class="input-group1">
-		                                    <span class="input-group-btn float-start">
-		                                        <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
-		                                          <span><i class="fas fa-minus"></i></span>
-		                                        </button>
-		                                    </span>
-		                                    <input type="text" id="quantity" name="quantity" class=" input-number" value="10">
-		                                    <span class="input-group-btn float-end">
-		                                        <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
-		                                            <span><i class="fas fa-plus"></i></span>
-		                                        </button>
-		                                    </span>
-	                                	</div>
-                        			</div>
-									<div class="clinic-details mt-4">
-										<div class="clinic-booking">
-											<a class="btn btn-primary" href="cart.html">Add To Cart</a>
-										</div>
-									</div>
-									<div class="card flex-fill mt-4 mb-0">
-										<ul class="list-group list-group-flush">
-											<li class="list-group-item text-gray-6">SKU	<span class="float-end">2023-02-0057</span></li>
-											<li class="list-group-item text-gray-6">Pack Size	<span class="float-end">100g</span></li>
-											<li class="list-group-item text-gray-6">Unit Count	<span class="float-end">200ml</span></li>
-											<li class="list-group-item text-gray-6">Country	<span class="float-end">Japan</span></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<div class="card search-filter">
-								<div class="card-body">
-									<div class="card flex-fill mt-0 mb-0">
-										<ul class="list-group list-group-flush benifits-col">
-											<li class="list-group-item d-flex align-items-center">
-												<div>
-													<i class="fas fa-shipping-fast"></i>
-												</div>
-												<div>
-													Free Shipping<br><span class="text-sm">For orders from $50</span>
-												</div>
-											</li>
-											<li class="list-group-item d-flex align-items-center">
-												<div>
-													<i class="far fa-question-circle"></i>
-												</div>
-												<div>
-													Support 24/7<br><span class="text-sm">Call us anytime</span>
-												</div>
-											</li>
-											<li class="list-group-item d-flex align-items-center">
-												<div>
-													<i class="fas fa-hands"></i>
-												</div>
-												<div>
-													100% Safety<br><span class="text-sm">Only secure payments</span>
-												</div>
-											</li>
-											<li class="list-group-item d-flex align-items-center">
-												<div>
-													<i class="fas fa-tag"></i>
-												</div>
-												<div>
-													Hot Offers<br><span class="text-sm">Discounts up to 90%</span>
-												</div>
-											</li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<!-- /Right Details -->
-							
 						</div>
 
 					</div>

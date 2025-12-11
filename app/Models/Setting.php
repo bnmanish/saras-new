b1a3eb5dfcc321abd7925a683f41e49fb1d72107
bnmanish <bnmanish006@gmail.com>
2025-12-11 08:04:32 +0530

    changes

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'primary_contact',
+        'secondary_contact', 
+        'primary_email',
+        'secondary_email',
+        'primary_address',
+        'secondary_address',
+        'copyrights',
+        'facebook',
+        'instagram',
+        'twitter',
+        'linkedin',
+        'youtube',
+        'head_content',
+        'site_logo',
+        'site_logo2',
+        'favicon'
+    ];
 }

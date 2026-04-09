8504c7aafdc8d00535f81b9bf06d4ec296150a1e
bnmanish <bnmanish006@gmail.com>
2026-04-10 00:39:11 +0530

    seeder update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,41 +2,41 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => 'B N Manish',
-            'email' => 'bnmanish006@gmail.com',
-            'user_name' => 'bnmanish',
-            'password' => Hash::make('12345'),
-            'mobile'    =>  '8116648011',
-            'role'  =>  'admin',
-            'email_verified'    =>  '1',
-            'mobile_verified'   =>  '1',
-            'status'    =>  '1',
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        User::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'user_name' => 'admin',
+            'password' => Hash::make('password'),
+            'mobile' => '8116648011',
+            'role' => 'admin',
+            'email_verified' => '1',
+            'mobile_verified' => '1',
+            'status' => '1',
         ]);
 
-        DB::table('users')->insert([
-            'name' => 'Ram Lal',
-            'email' => 'ramlal@gmail.com',
-            'user_name' => 'ramlal',
-            'password' => Hash::make('12345'),
-            'mobile'    =>  '9708669045',
-            'role'  =>  'user',
-            'email_verified'    =>  '1',
-            'mobile_verified'   =>  '1',
-            'status'    =>  '1',
+        User::create([
+            'name' => 'Test User',
+            'email' => 'user@example.com',
+            'user_name' => 'testuser',
+            'password' => Hash::make('password'),
+            'mobile' => '9708669045',
+            'role' => 'user',
+            'email_verified' => '1',
+            'mobile_verified' => '1',
+            'status' => '1',
         ]);
     }
 }
